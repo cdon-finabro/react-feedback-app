6f5b0a13755baa16df02e354776b702e547e4c08
cdonhofer <christian.donhofer@finabro.com>
2022-01-26 20:40:01 +0100

    css examples

--- src/FeedbackItem.jsx
+++ src/FeedbackItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./shared/Card";
 function FeedbackItem({ item }) {
   return (
-    <Card item={item}>
+    <Card item={item} reverse={true}>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
     </Card>
